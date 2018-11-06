e372694767272d79bff617ab8ffda313a146bedf
Ruslan Myaskovskiy <russ.myaskovskiy@gmail.com>
2018-11-05 20:40:44 -0800

    components correction

--- clicky-game/src/index.js
+++ clicky-game/src/index.js
@@ -2,6 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-export { default } from "./FriendCard";
 
 ReactDOM.render(<App />, document.getElementById("root"));
